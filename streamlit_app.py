3690e3076b59f3b8ace2e300ca689795a336480a
Ahmed-BS11 <ahmedbensalem201@gmail.com>
2023-11-04 16:45:49 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,7 +60,7 @@ if uploaded_image is not None:
     st.image(image, caption="Uploaded Image", use_column_width=True)
 
     # Load the InceptionV3 model
-    model = load_model("C:\\Users\\ahmed\\Downloads\\gnet.h5")
+    model = load_model(r"C:\Users\ahmed\Downloads\gnet.h5")
 
     # Preprocess the image
     # Resize the image to your desired dimensions
