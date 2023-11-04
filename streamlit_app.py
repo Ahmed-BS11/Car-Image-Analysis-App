2fdf155c32b7c612ddc8226344d08e9711d1eda2
Ahmed-BS11 <ahmedbensalem201@gmail.com>
2023-11-04 11:30:58 +0100

    creating the interface

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,13 +3,55 @@ import numpy as np
 import requests
 from PIL import Image
 
-# Page title and introduction
-st.title("AI-Generated Image Detector")
-st.write("Upload an image, and we'll determine if it's AI-generated or not.")
+# Set page title and favicon
+st.set_page_config(
+    page_title="AI Image Detector",
+    page_icon=":camera:",  # You can choose an appropriate emoji as the icon
+)
 
-# Upload image
-uploaded_image = st.file_uploader(
-    "Choose an image...", type=["jpg", "png", "jpeg"])
+# Custom CSS for styling
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-color: #f8f8f8;  /* Background color for the whole app */
+    }
+    .st-eb {
+        background-color: #0077b6;  /* Background color for the file uploader */
+        color: white;  /* Text color for the file uploader */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+# Center-align the title and intro text
+st.title("AI-Generated Image Detector")
+st.markdown(
+    "<p style='text-align: center;'>Upload an image, and we'll determine if it's AI-generated or not.</p>",
+    unsafe_allow_html=True,
+)
+
+# Upload image with custom styling
+uploaded_image = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"], key="file_uploader")
+
+# Add some spacing for better visual separation
+st.write("")
+
+# Optionally, you can add more text or explanations to guide the user
+st.markdown(
+    "#### Instructions",
+    unsafe_allow_html=True,
+)
+st.markdown(
+    "1. Click the 'Choose an image...' button to upload an image file (JPEG, PNG, or JPG).",
+    unsafe_allow_html=True,
+)
+st.markdown(
+    "2. We'll analyze the image and determine if it's AI-generated or not.",
+    unsafe_allow_html=True,
+)
+
 if uploaded_image is not None:
     # Display the uploaded image
     image = Image.open(uploaded_image)
