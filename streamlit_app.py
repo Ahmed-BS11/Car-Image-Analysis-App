c95886cb9600a60827a3a6ee668294be88febcf8
Ahmed-BS11 <ahmedbensalem201@gmail.com>
2023-11-09 12:57:33 +0100

    width changing

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,7 +54,7 @@ st.markdown(
 if uploaded_image is not None:
     # Display the uploaded image
     image = Image.open(uploaded_image)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", width=224)
 
     file_path = os.path.abspath("gnet.h5")
     model = load_model(file_path)
@@ -63,18 +63,19 @@ if uploaded_image is not None:
     # Resize the image to your desired dimensions
     img = image.resize((224, 224))
     img = np.array(img)
-    img = img / 255.0  # Apply the same rescaling as in your data generators
+    img = img / 255.0  
     img = np.expand_dims(img, axis=0)
 
-    # Make predictions
     prediction = model.predict(img)
     #st.write(f"Prediction: {prediction}")
     # Display the result
     if prediction > 0.5:
         result = "AI-Generated Image"
         st.markdown(f"<p style='font-size:60px;'>Prediction: {result}</p>", unsafe_allow_html=True)
-        st.write(f"Confidence: {prediction[0][0] * 100:.2f}%")
+        st.markdown(f"<p style='font-size:40px;'>Confidence: {prediction[0][0] * 100:.2f}%</p>", unsafe_allow_html=True)
+
     else:
         result = "Not AI-Generated Image"
-        st.markdown(f"<p style='font-size:40px;'>Prediction: {result}</p>", unsafe_allow_html=True)
-        st.write(f"Confidence: {100 - prediction[0][0] * 100:.2f}%")
+        st.markdown(f"<p style='font-size:60px;'>Prediction: {result}</p>", unsafe_allow_html=True)
+        st.markdown(f"<p style='font-size:40px;'>Confidence: {100-prediction[0][0] * 100:.2f}%</p>", unsafe_allow_html=True)
+
