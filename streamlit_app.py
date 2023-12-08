3a6983f026fea35c0670c3c21b2e280522b0f1c4
Ahmed-BS11 <ahmedbensalem201@gmail.com>
2023-12-08 18:51:58 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,35 +51,35 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
-st.sidebar.selectbox('Select Algorithm',['AIorNot','Damage Severity','Damaged Parts','Segmentation'])
+page=st.sidebar.selectbox('Select Algorithm',['AIorNot','Damage Severity','Damaged Parts','Segmentation'])
 st.sidebar.markdown("""---""")
 st.sidebar.write('Created by Faidi Hamza, Cherif Jawhar & Ben Salem Ahmed')
+if page == 'AIorNot':
+    if uploaded_image is not None:
+        # Display the uploaded image
+        image = Image.open(uploaded_image)
+        st.image(image, caption="Uploaded Image", use_column_width=True)
 
-if uploaded_image is not None:
-    # Display the uploaded image
-    image = Image.open(uploaded_image)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+        file_path = os.path.abspath("gnet.h5")
+        model = load_model(file_path)
 
-    file_path = os.path.abspath("gnet.h5")
-    model = load_model(file_path)
+        # Preprocess the image
+        # Resize the image to your desired dimensions
+        img = image.resize((224, 224))
+        img = np.array(img)
+        img = img / 255.0  
+        img = np.expand_dims(img, axis=0)
 
-    # Preprocess the image
-    # Resize the image to your desired dimensions
-    img = image.resize((224, 224))
-    img = np.array(img)
-    img = img / 255.0  
-    img = np.expand_dims(img, axis=0)
+        prediction = model.predict(img)
+        #st.write(f"Prediction: {prediction}")
+        # Display the result
+        if prediction > 0.5:
+            result = "AI-Generated Image"
+            st.markdown(f"<p style='font-size:60px;'>Prediction: {result}</p>", unsafe_allow_html=True)
+            st.markdown(f"<p style='font-size:40px;'>Confidence: {prediction[0][0] * 100:.2f}%</p>", unsafe_allow_html=True)
 
-    prediction = model.predict(img)
-    #st.write(f"Prediction: {prediction}")
-    # Display the result
-    if prediction > 0.5:
-        result = "AI-Generated Image"
-        st.markdown(f"<p style='font-size:60px;'>Prediction: {result}</p>", unsafe_allow_html=True)
-        st.markdown(f"<p style='font-size:40px;'>Confidence: {prediction[0][0] * 100:.2f}%</p>", unsafe_allow_html=True)
-
-    else:
-        result = "Not AI-Generated Image"
-        st.markdown(f"<p style='font-size:60px;'>Prediction: {result}</p>", unsafe_allow_html=True)
-        st.markdown(f"<p style='font-size:40px;'>Confidence: {100-prediction[0][0] * 100:.2f}%</p>", unsafe_allow_html=True)
+        else:
+            result = "Not AI-Generated Image"
+            st.markdown(f"<p style='font-size:60px;'>Prediction: {result}</p>", unsafe_allow_html=True)
+            st.markdown(f"<p style='font-size:40px;'>Confidence: {100-prediction[0][0] * 100:.2f}%</p>", unsafe_allow_html=True)
 
