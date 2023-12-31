ad11d1ca8442e393ccc81df2a988e935714ffc46
Ahmed-BS11 <ahmedbensalem201@gmail.com>
2023-12-31 19:46:46 +0100

    adding cost of repair

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from tensorflow.keras.optimizers import Adam
 import subprocess
 import uuid
 import glob
-import io
+import requests
 
 try:
     from roboflow import Roboflow
@@ -70,7 +70,113 @@ def load_automl():
     model = project.version(1).model
     return model
 
-page=st.sidebar.selectbox('Select Algorithm',['AIorNot','Damage Severity','Damaged Parts','Segmentation'])
+headers = {"Authorization": "Bearer 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"}
+API_URL1 = "https://api-inference.huggingface.co/models/dima806/car_brand_image_detection"
+API_URL2 = "https://api-inference.huggingface.co/models/beingamit99/car_damage_detection"
+
+def query(filename,API_URL):
+    with open(filename, "rb") as f:
+        data = f.read()
+    response = requests.post(API_URL, headers=headers, data=data)
+    return response.json()
+car_types = {
+    'Aston Martin': 'Luxury',
+    'Mercedes-Benz': 'Luxury',
+    'Mini': 'Standard',
+    'Tesla': 'Electric',
+    'GMC': 'SUV/Truck',
+    'Alfa Romeo': 'Sport',
+    'Studebaker': 'Classic',
+    'Suzuki': 'Compact',
+    'Peugeot': 'Standard',
+    'Genesis': 'Luxury',
+    'BMW': 'Luxury',
+    'Honda': 'Standard',
+    'Chrysler': 'Standard',
+    'Mazda': 'Standard',
+    'Infiniti': 'Luxury',
+    'Land Rover': 'SUV',
+    'Dodge': 'Standard',
+    'Fiat': 'Compact',
+    'Maserati': 'Luxury',
+    'Saab': 'Standard',
+    'Nissan': 'Standard',
+    'Hudson': 'Classic',
+    'Lincoln': 'Luxury',
+    'Volvo': 'Luxury',
+    'Mitsubishi': 'Standard',
+    'Oldsmobile': 'Classic',
+    'Lexus': 'Luxury',
+    'Buick': 'Luxury',
+    'Jaguar': 'Luxury',
+    'Toyota': 'Standard',
+    'Volkswagen': 'Standard',
+    'Renault': 'Standard',
+    'Citroen': 'Standard',
+    'Audi': 'Luxury',
+    'Subaru': 'Standard',
+    'Cadillac': 'Luxury',
+    'Pontiac': 'Standard',
+    'Porsche': 'Sport',
+    'Daewoo': 'Compact',
+    'Bugatti': 'Exotic',
+    'Jeep': 'SUV',
+    'Ram Trucks': 'Truck',
+    'Chevrolet': 'Standard',
+    'MG': 'Sport',
+    'Hyundai': 'Standard',
+    'Ferrari': 'Exotic',
+    'Acura': 'Luxury',
+    'Kia': 'Standard',
+    'Bentley': 'Luxury',
+    'Ford': 'Standard',
+}
+repair_cost_by_type = {
+    'Luxury': {
+        'Dent': {'Minor': 500, 'Moderate': 1000, 'Severe': 2000},
+        'Scratch': {'Minor': 300, 'Moderate': 700, 'Severe': 1500},
+        'Crack': {'Minor': 800, 'Moderate': 1200, 'Severe': 2500},
+        'Glass Shatter': {'Minor': 1000, 'Moderate': 1800, 'Severe': 3000},
+        'Lamp Broken': {'Minor': 600, 'Moderate': 1000, 'Severe': 2000},
+    },
+    'Standard': {
+        'Dent': {'Minor': 400, 'Moderate': 850, 'Severe': 1600},
+        'Scratch': {'Minor': 200, 'Moderate': 550, 'Severe': 1200},
+        'Crack': {'Minor': 700, 'Moderate': 1050, 'Severe': 2200},
+        'Glass Shatter': {'Minor': 800, 'Moderate': 1500, 'Severe': 2700},
+        'Lamp Broken': {'Minor': 500, 'Moderate': 900, 'Severe': 1800},
+    },
+    'Sport': {
+        'Dent': {'Minor': 600, 'Moderate': 1100, 'Severe': 2100},
+        'Scratch': {'Minor': 350, 'Moderate': 800, 'Severe': 1600},
+        'Crack': {'Minor': 900, 'Moderate': 1300, 'Severe': 2600},
+        'Glass Shatter': {'Minor': 1100, 'Moderate': 2000, 'Severe': 3200},
+        'Lamp Broken': {'Minor': 700, 'Moderate': 1200, 'Severe': 2300},
+    },
+    'Electric': {
+        'Dent': {'Minor': 700, 'Moderate': 1200, 'Severe': 2300},
+        'Scratch': {'Minor': 400, 'Moderate': 900, 'Severe': 1800},
+        'Crack': {'Minor': 1000, 'Moderate': 1400, 'Severe': 2700},
+        'Glass Shatter': {'Minor': 1200, 'Moderate': 2200, 'Severe': 3400},
+        'Lamp Broken': {'Minor': 800, 'Moderate': 1300, 'Severe': 2400},
+    },
+    'SUV': {
+        'Dent': {'Minor': 500, 'Moderate': 950, 'Severe': 1800},
+        'Scratch': {'Minor': 250, 'Moderate': 600, 'Severe': 1300},
+        'Crack': {'Minor': 800, 'Moderate': 1100, 'Severe': 2200},
+        'Glass Shatter': {'Minor': 900, 'Moderate': 1600, 'Severe': 2800},
+        'Lamp Broken': {'Minor': 550, 'Moderate': 1000, 'Severe': 2000},
+    },
+    'Classic': {
+        'Dent': {'Minor': 300, 'Moderate': 700, 'Severe': 1500},
+        'Scratch': {'Minor': 150, 'Moderate': 500, 'Severe': 1100},
+        'Crack': {'Minor': 600, 'Moderate': 1000, 'Severe': 2000},
+        'Glass Shatter': {'Minor': 700, 'Moderate': 1300, 'Severe': 2500},
+        'Lamp Broken': {'Minor': 400, 'Moderate': 800, 'Severe': 1700},
+    },
+    # Add more types and their repair cost details...
+}
+page=st.sidebar.selectbox('Select Algorithm',['AIorNot','Damage Severity','Damaged Parts','Repair cost'])
 st.sidebar.markdown("""---""")
 st.sidebar.write('Created by Faidi Hamza, Cherif Jawhar & Ben Salem Ahmed')
 
@@ -186,3 +292,27 @@ if page == 'Damaged Parts':
 
             # Display the prediction image in Streamlit
             col2.image("prediction.jpg", caption="Prediction Image", use_column_width=True)
+
+
+if page == 'Repair cost':
+    upload_columns = st.columns([2, 1])
+    file_upload = upload_columns[0].expander(label='Upload Your Image')
+    uploaded_image = file_upload.file_uploader("Choose an image...", type=["jpg", "png", "jpeg", 'webp'], key="file_uploader")
+
+    if uploaded_image is not None:
+        # Display the uploaded image
+        image = Image.open(uploaded_image)
+        upload_columns[1].image(image, caption="Uploaded Image", use_column_width=True, width=600)
+        
+        # Add a button for prediction
+        col1, col2, col3 = st.columns([1,1,1]) # this used to center the button
+        if col2.button("Predict repair cost"):
+            temp_dir = "temp_images"
+            os.makedirs(temp_dir, exist_ok=True)
+            temp_image_path = os.path.join(temp_dir, f"uploaded_image_{uuid.uuid4()}.jpg")
+            image.save(temp_image_path, format='JPEG')
+            brand=query(temp_image_path,API_URL1)[0]['label']
+            damage=query(temp_image_path,API_URL2)[0]['label']
+            st.markdown(f"<p style='font-size:60px;'>Your car is a: {brand}, which is a {car_types[brand]} car.
+                        Your car has a {damage} damage</p>", unsafe_allow_html=True)
+
